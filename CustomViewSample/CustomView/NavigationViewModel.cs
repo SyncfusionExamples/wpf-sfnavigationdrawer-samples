add70ea362dab83ce0b9d86c79c4f5510e5779fb
Paul Anderson <42271912+PaulAnderson02@users.noreply.github.com>
2021-01-27 10:37:54 +0530

    Samples added

---
+++ CustomViewSample/CustomView/NavigationViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomView
+{
+    public class NavigationViewModel
+    {
+        public NavigationViewModel()
+        {
+            Contents = new List<MailModel>();
+
+            Contents.Add(new MailModel() { Name = "Home" });
+            Contents.Add(new MailModel() { Name = "Profile" });
+            Contents.Add(new MailModel() { Name = "Inbox" });
+            Contents.Add(new MailModel() { Name = "Outbox" });
+            Contents.Add(new MailModel() { Name = "Sent" });
+            Contents.Add(new MailModel() { Name = "Trash" });
+        }
+
+        public List<MailModel> Contents { get; set; }
+    }
+}
---
+++ ItemsSourceSample/NavigationDrawer_ItemsSource/MusicModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavigationDrawer_ItemsSource
+{
+    public class MusicModel
+    {
+        private string item;
+        public string Item
+        {
+            get { return item; }
+            set { item = value; }
+        }
+
+        private object icon;
+        public object Icon
+        {
+            get { return icon; }
+            set { icon = value; }
+        }
+    }
+}
