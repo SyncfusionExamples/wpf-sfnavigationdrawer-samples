4644bf85613a46fb2c4246ddfe54734e1e0571e3
Paul Anderson <42271912+PaulAnderson02@users.noreply.github.com>
2021-03-31 09:21:32 +0530

    New samples committed

---
+++ Hierarchical_Data_Binding/Hierarchical_Data_Binding/Model.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hierarchical_Data_Binding
+{
+    public class Model
+    {
+        public ObservableCollection<Model> SubItems { get; set; }
+
+        private string item;
+        public string Item
+        {
+            get { return item; }
+            set { item = value; }
+        }
+
+        private object icon;
+        public object Icon
+        {
+            get { return icon; }
+            set { icon = value; }
+        }
+    }
+}
