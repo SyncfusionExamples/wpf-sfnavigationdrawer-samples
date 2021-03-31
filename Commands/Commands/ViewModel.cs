d3c94549b087a421f1bc9b2bba395ace5e054e88
Paul Anderson <42271912+PaulAnderson02@users.noreply.github.com>
2021-03-31 09:20:33 +0530

    New samples for Navigationdrawer

---
+++ Commands/Commands/ViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Commands
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private bool _canperformaction = true;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        public ViewModel()
+        {
+            ClickCommand = new DelegateCommand<object>(ClickAction, CanPerformClickAction);
+        }
+        public bool CanPerformAction
+        {
+            get
+            {
+                return _canperformaction;
+            }
+            set
+            {
+                _canperformaction = value;
+                this.ClickCommand.RaiseCanExecuteChanged();
+                this.OnPropertyChanged("CanPerformAction");
+            }
+        }
+
+        private bool CanPerformClickAction(object parameter)
+        {
+            return CanPerformAction;
+        }
+
+        public DelegateCommand<object> ClickCommand { get; set; }
+
+        private void ClickAction(object parameter)
+        {
+            MessageBox.Show(parameter.ToString() + " item has been clicked");
+        }
+
+    }
+}
---
+++ Custom_View/Custom_View/ViewModelcs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_View
+{
+    public class ViewModel
+    {
+        public ViewModel()
+        {
+            Contents = new List<Model>();
+
+            Contents.Add(new Model() { Name = "Home" });
+            Contents.Add(new Model() { Name = "Profile" });
+            Contents.Add(new Model() { Name = "Inbox" });
+            Contents.Add(new Model() { Name = "Outbox" });
+            Contents.Add(new Model() { Name = "Sent" });
+            Contents.Add(new Model() { Name = "Trash" });
+            Contents.Add(new Model() { Name = "Sign Out" });
+        }
+
+        public List<Model> Contents { get; set; }
+    }
+}
