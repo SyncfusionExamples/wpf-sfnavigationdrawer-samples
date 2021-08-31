34e9ce9019942b2ae10b3b7bb80432c6c7bdd7da
Paul Anderson <42271912+PaulAnderson02@users.noreply.github.com>
2021-08-31 12:09:05 +0530

    How-to-add-content-to-each-NavigationItem

---
+++ How-to-add-content-to-each-NavigationItem-using-MVVM/NavigationSample/NavigationSample/Helper/Helper.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NavigationSample
+{
+    public class DelegateCommand<T> : ICommand
+    {
+        private Predicate<T> _canExecute;
+        private Action<T> _method;
+        bool _canExecuteCache = true;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DelegateCommand"/> class.
+        /// </summary>
+        /// <param name="method">The method.</param>
+        public DelegateCommand(Action<T> method)
+            : this(method, null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DelegateCommand"/> class.
+        /// </summary>
+        /// <param name="method">The method.</param>
+        /// <param name="canExecute">The can execute.</param>
+        public DelegateCommand(Action<T> method, Predicate<T> canExecute)
+        {
+            _method = method;
+            _canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// Defines the method that determines whether the command can execute in its current state.
+        /// </summary>
+        /// <param name="parameter">Data used by the command.  If the command does not require data to be passed, this object can be set to null.</param>
+        /// <returns>
+        /// true if this command can be executed; otherwise, false.
+        /// </returns>
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute != null)
+            {
+                bool tempCanExecute = _canExecute((T)parameter);
+
+                if (_canExecuteCache != tempCanExecute)
+                {
+                    _canExecuteCache = tempCanExecute;
+                    this.RaiseCanExecuteChanged();
+                }
+            }
+
+            return _canExecuteCache;
+        }
+
+        /// <summary>
+        /// Raises CanExecuteChanged event to notify changes in command status.
+        /// </summary>
+        public void RaiseCanExecuteChanged()
+        {
+            if (CanExecuteChanged != null)
+            {
+                CanExecuteChanged(this, new EventArgs());
+            }
+        }
+
+        /// <summary>
+        /// Defines the method to be called when the command is invoked.
+        /// </summary>
+        /// <param name="parameter">Data used by the command.  If the command does not require data to be passed, this object can be set to null.</param>
+        public void Execute(object parameter)
+        {
+            if (_method != null)
+                _method.Invoke((T)parameter);
+        }
+
+        #region ICommand Members
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+
+        #endregion
+    }
+}
---
+++ How-to-add-content-to-each-NavigationItem-using-MVVM/NavigationSample/NavigationSample/ViewModel/ViewModel.cs
@@ -0,0 +1,64 @@
+﻿using NavigationSample.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace NavigationSample
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private ContentControl contentControl;
+        private bool _canperformaction = true;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        public ViewModel(ContentControl content)
+        {
+            contentControl = content;
+            ClickCommand = new DelegateCommand<object>(ClickAction, CanPerformClickAction);
+        }
+        public bool CanPerformAction
+        {
+            get
+            {
+                return _canperformaction;
+            }
+            set
+            {
+                _canperformaction = value;
+                this.ClickCommand.RaiseCanExecuteChanged();
+                this.OnPropertyChanged("CanPerformAction");
+            }
+        }
+
+        private bool CanPerformClickAction(object parameter)
+        {
+            return CanPerformAction;
+        }
+
+        public DelegateCommand<object> ClickCommand { get; set; }
+
+        private void ClickAction(object parameter)
+        {
+            if ((string)parameter == "Home")
+            {
+                contentControl.Content = new HomeView();
+            }
+            else if ((string)parameter == "Books")
+            {
+                contentControl.Content = new BooksView();
+            }
+        }
+
+    }
+}
